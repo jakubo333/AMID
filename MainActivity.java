16ef7eae9604ffc036ac8813be06fe481fa6e92f
jakubo333 <jakubolesinski98@gmail.com>
2025-11-27 17:39:26 +0100

    MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,30 +1,83 @@
-package com.example.toast;
+package com.example.lab2;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Panel chessPanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chessPanel = findViewById(R.id.chess_panel);
 
 
-        Button btnImieNazwisko = findViewById(R.id.btnImieNazwisko);
+        registerForContextMenu(chessPanel);
+    }
 
 
-        btnImieNazwisko.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
 
-                Toast.makeText(getApplicationContext(),
-                        "Jakub Oleśiński",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_reset) {
+            chessPanel.resetBoard();
+            Toast.makeText(this, "Szachownica zresetowana", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_invert) {
+            chessPanel.invertColors();
+            Toast.makeText(this, "Kolory odwrócone", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_about) {
+            Toast.makeText(this, "Szachownica - laboratorium", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        menu.setHeaderTitle("Menu kontekstowe");
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_reset) {
+            chessPanel.resetBoard();
+            Toast.makeText(this, "Szachownica zresetowana ", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_invert) {
+            chessPanel.invertColors();
+            Toast.makeText(this, "Kolory odwrócone ", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_about) {
+            Toast.makeText(this, "Szachownica - laboratorium", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onContextItemSelected(item);
     }
 }
