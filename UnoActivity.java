060a01757ff6978e4d2d5a66c78647e7f22035ba
jakubo333 <jakubolesinski98@gmail.com>
2026-02-01 03:24:58 +0100

    Update and rename Panel.java to UnoActivity.java

---
+++ UnoActivity.java
@@ -0,0 +1,12 @@
+
+package com.example.lab2;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UnoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uno);
+    }
+}
