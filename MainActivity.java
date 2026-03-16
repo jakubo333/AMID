ceb3297c52ca16e2bbef4628412732174df96683
jakubo333 <jakubolesinski98@gmail.com>
2026-03-16 14:05:13 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,83 +1,30 @@
-package com.example.lab2;
+package com.example.lab7;
 
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Panel chessPanel;
+    Button buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        chessPanel = findViewById(R.id.chess_panel);
 
+        buttonNext = findViewById(R.id.buttonNext);
 
-        registerForContextMenu(chessPanel);
-    }
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_reset) {
-            chessPanel.resetBoard();
-            Toast.makeText(this, "Szachownica zresetowana", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.action_invert) {
-            chessPanel.invertColors();
-            Toast.makeText(this, "Kolory odwrócone", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.action_about) {
-            Toast.makeText(this, "Szachownica - laboratorium", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        menu.setHeaderTitle("Menu kontekstowe");
-    }
-
-
-    @Override
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_reset) {
-            chessPanel.resetBoard();
-            Toast.makeText(this, "Szachownica zresetowana ", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.action_invert) {
-            chessPanel.invertColors();
-            Toast.makeText(this, "Kolory odwrócone ", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.action_about) {
-            Toast.makeText(this, "Szachownica - laboratorium", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onContextItemSelected(item);
+            }
+        });
     }
 }
---
+++ SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.lab7;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button buttonBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        buttonBack = findViewById(R.id.buttonBack);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
+    }
+}
