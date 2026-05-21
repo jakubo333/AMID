12bf19063d220857ee8cff3497c1120bed896186
jakubo333 <jakubolesinski98@gmail.com>
2026-05-21 14:52:18 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,23 @@
-package com.example.toast;
+package com.example.laboop;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editBrand, editModel, editYear;
+    Button buttonAdd;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,17 +25,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button btnImieNazwisko = findViewById(R.id.btnImieNazwisko);
+        editBrand = findViewById(R.id.editBrand);
+        editModel = findViewById(R.id.editModel);
+        editYear = findViewById(R.id.editYear);
+
+        buttonAdd = findViewById(R.id.buttonAdd);
+        textResult = findViewById(R.id.textResult);
 
 
-        btnImieNazwisko.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        buttonAdd.setOnClickListener(v -> {
 
-                Toast.makeText(getApplicationContext(),
-                        "Jakub Oleśiński",
-                        Toast.LENGTH_SHORT).show();
-            }
+            String brand = editBrand.getText().toString();
+            String model = editModel.getText().toString();
+            String yearText = editYear.getText().toString();
+
+
+
+
+            int year = Integer.parseInt(yearText);
+
+
+            Car car = new Car(brand, model, year);
+
+
+            textResult.setText(car.getCarDetails());
         });
     }
 }
